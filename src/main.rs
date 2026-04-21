86c686e81977a61e15cd853cb8b6d4632d858e6f
abominablepug <esnahoum@gmail.com>
2026-04-21 00:43:31 -0400

    style: changed display text

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,33 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello, World!";
+static PULSARTOS: &[u8] =
+    b"Welcome to PulsaRTOS!\nA simple real-time operating system written in Rust.";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    let mut col = 0;
+    let mut row = 0;
+
+    for &byte in PULSARTOS.iter() {
+        if byte == b'\n' {
+            col = 0;
+            row += 1;
+        } else {
+            let offset = (row * 80 + col) * 2;
+            unsafe {
+                *vga_buffer.offset(offset) = byte;
+                *vga_buffer.offset(offset + 1) = 0xb;
+            }
+
+            col += 1;
+
+            if col >= 80 {
+                col = 0;
+                row += 1;
+            }
         }
     }
 
