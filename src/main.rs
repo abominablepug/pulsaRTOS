bbe5f0e3de6bd79be133de7e8bd6f43831eef8ef
abominablepug <esnahoum@gmail.com>
2026-04-22 04:32:06 -0400

    feat: print and println macro support

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,9 @@ use core::panic::PanicInfo;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER
-        .lock()
-        .write_str("Welcome to PulsaRTOS!\nA simple real-time operating system written in Rust.\n")
-        .unwrap();
-    write!(
-        vga_buffer::WRITER.lock(),
-        "Insertions like {} and {} have now been added!",
-        7 * 6,
-        1.337
-    )
-    .unwrap();
+    println!(
+        "Welcome to PulsaRTOS!\nA simple real-time operating system written in Rust.\nNow with support for print and println macros!"
+    );
 
     loop {}
 }
