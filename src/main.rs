7fc875d7a1c4507295c3097707ebb2cd20c04dcd
abominablepug <esnahoum@gmail.com>
2026-04-22 04:21:11 -0400

    feat: global write variable (simplified vga_buffer printing)

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,24 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+mod vga_buffer;
 
-static PULSARTOS: &[u8] =
-    b"Welcome to PulsaRTOS!\nA simple real-time operating system written in Rust.";
+use core::panic::PanicInfo;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    let mut col = 0;
-    let mut row = 0;
-
-    for &byte in PULSARTOS.iter() {
-        if byte == b'\n' {
-            col = 0;
-            row += 1;
-        } else {
-            let offset = (row * 80 + col) * 2;
-            unsafe {
-                *vga_buffer.offset(offset) = byte;
-                *vga_buffer.offset(offset + 1) = 0xb;
-            }
-
-            col += 1;
-
-            if col >= 80 {
-                col = 0;
-                row += 1;
-            }
-        }
-    }
+    use core::fmt::Write;
+    vga_buffer::WRITER
+        .lock()
+        .write_str("Welcome to PulsaRTOS!\nA simple real-time operating system written in Rust.\n")
+        .unwrap();
+    write!(
+        vga_buffer::WRITER.lock(),
+        "Insertions like {} and {} have now been added!",
+        7 * 6,
+        1.337
+    )
+    .unwrap();
 
     loop {}
 }
