3ca52853f569c50c894b46644f0c6c5e8ea457f5
abominablepug <esnahoum@gmail.com>
2026-04-22 04:35:20 -0400

    feat: panic error message printed to buffer

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ use core::panic::PanicInfo;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!(
-        "Welcome to PulsaRTOS!\nA simple real-time operating system written in Rust.\nNow with support for print and println macros!"
-    );
+    println!("Welcome to PulsaRTOS!\nA simple real-time operating system written in Rust.\n");
+    panic!("NOW ABLE TO PANIC!!!");
 
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
