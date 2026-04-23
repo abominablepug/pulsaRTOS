9b6819f1f73a98f0036eff54aeaaf96b2f4ef638
abominablepug <esnahoum@gmail.com>
2026-04-23 00:00:02 -0400

    test: added simple vga_buffer tests

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -140,3 +140,25 @@ pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
 }
+
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple successful output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many successful output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Single line test string";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
