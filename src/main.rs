9cba7535871aa41374d2256f67dfbc70e7e78577
abominablepug <esnahoum@gmail.com>
2026-04-23 00:19:47 -0400

    feat: implementation test architecture created

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,20 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(pulsaRTOS::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod serial;
-mod vga_buffer;
-
 use core::panic::PanicInfo;
+use pulsaRTOS::println;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    println!("Welcome to PulsaRTOS!\nA simple real-time operating system written in Rust.\n");
 
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
+    #[cfg(test)]
+    test_main();
 
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+    loop {}
 }
 
 #[cfg(not(test))]
@@ -64,20 +27,5 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    println!("Welcome to PulsaRTOS!\nA simple real-time operating system written in Rust.\n");
-
-    #[cfg(test)]
-    test_main();
-
-    panic!("NOW ABLE TO PANIC!!!");
-
-    loop {}
+    pulsaRTOS::test_panic_handler(info)
 }
---
+++ tests/basic_boot.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(pulsaRTOS::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use pulsaRTOS::println;
+
+fn test_runner(tests: &[&dyn Fn()]) {
+    unimplemented!();
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    pulsaRTOS::test_panic_handler(info)
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println successfult output");
+}
