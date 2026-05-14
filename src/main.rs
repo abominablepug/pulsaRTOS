a8bcc6955076b33556d3618472aa9223e1636574
abominablepug <esnahoum@gmail.com>
2026-05-14 13:35:02 -0400

    feat: prevent triple fault edge cases using stack switching

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,11 @@ pub extern "C" fn _start() -> ! {
 
     pulsaRTOS::init();
 
-    unsafe {
-        *(0xbadadd as *mut u32) = 42;
+    fn stack_overflow() {
+        stack_overflow();
     }
+
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
