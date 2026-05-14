fc6cef69189131a5e79df6518cc78f6808d16484
abominablepug <esnahoum@gmail.com>
2026-05-14 18:36:31 -0400

    feat: add pic8259 and timer interrupt handling

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,6 @@ pub extern "C" fn _start() -> ! {
 
     pulsaRTOS::init();
 
-    fn stack_overflow() {
-        stack_overflow();
-    }
-
-    stack_overflow();
-
     #[cfg(test)]
     test_main();
 
