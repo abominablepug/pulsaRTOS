7c1b9fd7878c2c0c0a369573676aaacca6a4968b
abominablepug <esnahoum@gmail.com>
2026-05-16 18:13:28 -0400

    feat: implement BootInfoFrameAllocator for kernel memory management

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use pulsaRTOS::memory;
+    use pulsaRTOS::memory::BootInfoFrameAllocator;
     use x86_64::{VirtAddr, structures::paging::Page};
 
     println!("Welcome to PulsaRTOS!\nA simple real-time operating system written in Rust.\n");
@@ -21,7 +22,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = memory::EmptyFrameAllocator;
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
     let page = Page::containing_address(VirtAddr::new(0));
     memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
