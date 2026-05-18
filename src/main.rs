e0d480fb2826408640801da6cf54a5b93e4263d7
abominablepug <esnahoum@gmail.com>
2026-05-18 02:23:11 -0400

    feat: add linked list allocator and real heap management

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,19 @@
 #![test_runner(pulsaRTOS::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use bootloader::{BootInfo, entry_point};
-use core::panic::PanicInfo;
+use core::{ffi::c_long, panic::PanicInfo};
 use pulsaRTOS::println;
 use x86_64::VirtAddr;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use pulsaRTOS::memory;
-    use pulsaRTOS::memory::BootInfoFrameAllocator;
-    use x86_64::{VirtAddr, structures::paging::Page};
+    use pulsaRTOS::allocator;
+    use pulsaRTOS::memory::{self, BootInfoFrameAllocator};
 
     println!("Welcome to PulsaRTOS!\nA simple real-time operating system written in Rust.\n");
 
@@ -24,13 +26,28 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe {
-        page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e);
+    let heap_value = Box::new(41);
+    println!("heap value at {:p}", heap_value);
+
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
     }
+    println!("vec at {:p}", vec.as_slice());
+
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current ref count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "current ref count is now {}",
+        Rc::strong_count(&cloned_reference)
+    );
 
     #[cfg(test)]
     test_main();
